404ebf440b28f0f2bd3cfe076925886ff62c8c32
Henri Tunkkari <henri.tunkkari@hotmail.com>
2021-07-11 01:54:38 +0300

    Add data and initial config

---
+++ src/static/config.js
@@ -0,0 +1,10 @@
+/* Manually set obstacle heights in degrees */
+const obstacleHeights = {
+    "very low" : 5,
+    "low" : 20,
+    "medium" : 30,
+    "high" : 50,
+    "very high" : 70
+}
+
+export default obstacleHeights;
