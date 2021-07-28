a1bb857405c7547c7d58e809047faa8e83886db2
Henri Tunkkari <henri.tunkkari@hotmail.com>
2021-07-28 23:50:20 +0300

    Updates

---
+++ precache-manifest.37bd18d23746d00cd76874a754b383a1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09530ecd74cbbb1469273bcef7365ca7",
+    "url": "/grillspots/index.html"
+  },
+  {
+    "revision": "b6b20fa29f3c410305d8",
+    "url": "/grillspots/static/css/main.a5842c08.chunk.css"
+  },
+  {
+    "revision": "b5ceadd2b79771d55fac",
+    "url": "/grillspots/static/js/2.4fb2976e.chunk.js"
+  },
+  {
+    "revision": "ce37b6cbc143da1c6500d1266f84c9a3",
+    "url": "/grillspots/static/js/2.4fb2976e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b20fa29f3c410305d8",
+    "url": "/grillspots/static/js/main.e719c798.chunk.js"
+  },
+  {
+    "revision": "4ad050c5428d29f9b24c",
+    "url": "/grillspots/static/js/runtime-main.aa4d0fdb.js"
+  },
+  {
+    "revision": "98500b55da14cf6dd631909c03eb5fe3",
+    "url": "/grillspots/static/media/bench-disabled.98500b55.svg"
+  },
+  {
+    "revision": "5cea9a305348c30acf682c1e49db9847",
+    "url": "/grillspots/static/media/bench-white.5cea9a30.svg"
+  },
+  {
+    "revision": "12d600f63ab1970a5ef25c097372acf1",
+    "url": "/grillspots/static/media/grill-disabled.12d600f6.svg"
+  },
+  {
+    "revision": "c42351e1836caf6d9432ba3ea191301f",
+    "url": "/grillspots/static/media/grill-icon-undefined.c42351e1.png"
+  },
+  {
+    "revision": "e816ee44461d3eddf338be81591c5107",
+    "url": "/grillspots/static/media/grill-white.e816ee44.svg"
+  },
+  {
+    "revision": "e9ee5e0d9e5477c8094aa63bba4fb97b",
+    "url": "/grillspots/static/media/navigate-white.e9ee5e0d.svg"
+  },
+  {
+    "revision": "f7ff234225e3c363fb0b04de31672e56",
+    "url": "/grillspots/static/media/table-disabled.f7ff2342.svg"
+  },
+  {
+    "revision": "71423df4418d2d4a33e6e64ff437caa6",
+    "url": "/grillspots/static/media/table-white.71423df4.svg"
+  },
+  {
+    "revision": "4b9869a6859846e524d1985de86894f7",
+    "url": "/grillspots/static/media/wi-day-sunny.4b9869a6.svg"
+  }
+]);
