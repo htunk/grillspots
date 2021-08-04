ef25ec5610efc8d954cacb49717bb26ef4dfd3fd
Henri Tunkkari <henri.tunkkari@hotmail.com>
2021-08-04 15:48:38 +0300

    Updates

---
+++ precache-manifest.7102e00e270a35e1479275013f9fbb7c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ee6d829fc9c2309cb1ddcbcf10a1af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24542b710fd7eff0eebe",
+    "url": "/static/css/main.c799ad38.chunk.css"
+  },
+  {
+    "revision": "b5ceadd2b79771d55fac",
+    "url": "/static/js/2.4fb2976e.chunk.js"
+  },
+  {
+    "revision": "ce37b6cbc143da1c6500d1266f84c9a3",
+    "url": "/static/js/2.4fb2976e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24542b710fd7eff0eebe",
+    "url": "/static/js/main.dd245743.chunk.js"
+  },
+  {
+    "revision": "98faa1f90ba787d3562e",
+    "url": "/static/js/runtime-main.203a6646.js"
+  },
+  {
+    "revision": "98500b55da14cf6dd631909c03eb5fe3",
+    "url": "/static/media/bench-disabled.98500b55.svg"
+  },
+  {
+    "revision": "5cea9a305348c30acf682c1e49db9847",
+    "url": "/static/media/bench-white.5cea9a30.svg"
+  },
+  {
+    "revision": "12d600f63ab1970a5ef25c097372acf1",
+    "url": "/static/media/grill-disabled.12d600f6.svg"
+  },
+  {
+    "revision": "c42351e1836caf6d9432ba3ea191301f",
+    "url": "/static/media/grill-icon-undefined.c42351e1.png"
+  },
+  {
+    "revision": "e816ee44461d3eddf338be81591c5107",
+    "url": "/static/media/grill-white.e816ee44.svg"
+  },
+  {
+    "revision": "e9ee5e0d9e5477c8094aa63bba4fb97b",
+    "url": "/static/media/navigate-white.e9ee5e0d.svg"
+  },
+  {
+    "revision": "f7ff234225e3c363fb0b04de31672e56",
+    "url": "/static/media/table-disabled.f7ff2342.svg"
+  },
+  {
+    "revision": "71423df4418d2d4a33e6e64ff437caa6",
+    "url": "/static/media/table-white.71423df4.svg"
+  },
+  {
+    "revision": "4b9869a6859846e524d1985de86894f7",
+    "url": "/static/media/wi-day-sunny.4b9869a6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grillspots/precache-manifest.364578d11e4bec0e1a443ce202b866f4.js"
+  "/precache-manifest.7102e00e270a35e1479275013f9fbb7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/grillspots/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
